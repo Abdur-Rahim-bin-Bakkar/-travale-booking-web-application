3b85532904d8b1923b4103e8415199234d74c0e9
webdesignrahim4061 <webdesignrahim4061@gmail.com>
2026-05-11 10:35:11 +0600

    project init

---
+++ src/app/page.jsx
@@ -0,0 +1,11 @@
+import Navbar from "@/components/share/Navbar";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="">
+      <h1>this is main heading</h1>
+      <Navbar/>
+    </div>
+  );
+}
---
+++ src/components/share/Navbar.jsx
@@ -0,0 +1,12 @@
+import { Button } from '@heroui/react';
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <div>
+            <Button>Click Me</Button>
+        </div>
+    );
+};
+
+export default Navbar;
